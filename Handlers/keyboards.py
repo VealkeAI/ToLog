aa9631e09381ca436aa8baa1349e1efd2eb30b8e
Vealke ⠀ <koristaosu1337@gmail.com>
2025-11-22 22:54:36 +0000

    Kb: Updated

--- Handlers/keyboards.py
+++ Handlers/keyboards.py
@@ -15,12 +15,15 @@ start_kb = ReplyKeyboardMarkup(
         [
             KeyboardButton(text="Напоминания📕"),
             KeyboardButton(text="помощь📒")
+        ],
+        [
+            KeyboardButton(text="Настройки⚙️")
         ]
     ],
     resize_keyboard=True,
     input_field_placeholder="Выберите опцию...",
     selective=True,
-    one_time_keyboard=True
+    one_time_keyboard=False
 )
 
 admin_kb = ReplyKeyboardMarkup(
@@ -49,6 +52,104 @@ set_time_kb = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True,
     input_field_placeholder="Выберите длительность...",
+    selective=True,
+    one_time_keyboard=False
+)
+
+timezone_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Часовой-пояс🌏")
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите настройку...",
+    selective=True,
+    one_time_keyboard=False
+)
+
+tz_page_1 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Europe/Kaliningrad"),
+            KeyboardButton(text="Europe/Moscow"),
+            KeyboardButton(text="Europe/Kirov")
+        ],
+        [
+            KeyboardButton(text="Europe/Volgograd"),
+            KeyboardButton(text="Europe/Astrakhan"),
+            KeyboardButton(text="Europe/Saratov")
+        ],
+        [
+            KeyboardButton(text="Europe/Ulyanovsk"),
+            KeyboardButton(text="Europe/Samara"),
+            KeyboardButton(text="Asia/Yekaterinburg")
+        ],
+        [
+            KeyboardButton(text="Страница №2")
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите настройку...",
+    selective=True,
+    one_time_keyboard=False
+)
+
+tz_page_2 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Asia/Omsk"),
+            KeyboardButton(text="Asia/Novosibirsk"),
+            KeyboardButton(text="Asia/Barnaul")
+        ],
+        [
+            KeyboardButton(text="Asia/Tomsk"),
+            KeyboardButton(text="Asia/Novokuznetsk"),
+            KeyboardButton(text="Asia/Krasnoyarsk")
+        ],
+        [
+            KeyboardButton(text="Asia/Irkutsk"),
+            KeyboardButton(text="Asia/Chita"),
+            KeyboardButton(text="Asia/Yakutsk")
+        ],
+        [
+            KeyboardButton(text="Страница №3")
+        ],
+        [
+            KeyboardButton(text="🔙 Прошлая страница")
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите настройку...",
+    selective=True,
+    one_time_keyboard=False
+)
+
+tz_page_3 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Asia/Khandyga"),
+            KeyboardButton(text="Asia/Vladivostok"),
+            KeyboardButton(text="Asia/Ust-Nera")
+        ],
+        [
+            KeyboardButton(text="Asia/Magadan"),
+            KeyboardButton(text="Asia/Sakhalin"),
+            KeyboardButton(text="Asia/Srednekolymsk")
+        ],
+        [
+            KeyboardButton(text="Asia/Kamchatka"),
+            KeyboardButton(text="Asia/Anadyr"),
+        ],
+        [
+            KeyboardButton(text="❌ Отмена")
+        ],
+        [
+            KeyboardButton(text="🔙 Прошлая страница")
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите настройку...",
     selective=True,
     one_time_keyboard=False
 )
