a55f112f01f7af5ef7aca8547266dbebbfc79a17
Vealke ⠀ <koristaosu1337@gmail.com>
2025-11-22 12:28:31 +0000

    Styled main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from Handlers import start
-from Handlers.commands import help
+from Handlers.commands import help, tasks
 from Handlers.root import admin
+from rich.console import Console
 
+console = Console()
 load_dotenv()
 
 async def main():
@@ -17,10 +19,12 @@ async def main():
 
     dp.include_routers(
         start.router,
-        admin.router,
-        help.router
+        help.router,
+        tasks.router,
+        admin.router
     )
-
+    console.print(f"[bold blue]Bot ID: {bot.id}\n" \
+                    "Bot is running...[/bold blue]")
     await dp.start_polling(bot, skip_updates="false")
 
 if __name__ == "__main__":
